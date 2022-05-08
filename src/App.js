521a4b89a19d996265ac1bd911184420f6e78d11
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-08 18:18:28 +0300

    Initalized Search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-    {
-        title: "What is React?",
-        content: "React is a front end javascript framework",
-    },
-    {
-        title: "Why use React?",
-        content: "React is a favorite JS library among engineers",
-    },
-    {
-        title: "How do you use React?",
-        content: "You use React by creating components",
-    },
-];
+// const items = [
+//     {
+//         title: "What is React?",
+//         content: "React is a front end javascript framework",
+//     },
+//     {
+//         title: "Why use React?",
+//         content: "React is a favorite JS library among engineers",
+//     },
+//     {
+//         title: "How do you use React?",
+//         content: "You use React by creating components",
+//     },
+// ];
 
 const App = () => {
     return (
         <div>
-            <Accordion items={items} />
+            <Search />
         </div>
     );
 };
