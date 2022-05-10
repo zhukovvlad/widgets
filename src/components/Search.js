a26562214325661659a11bf872649dec889c6d7b
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-10 15:53:16 +0300

    Added useEffect and axios request to the Wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-    return <h1>Seacrh</h1>
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+        };
+
+        search();
+    }, [term]);
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Search;
