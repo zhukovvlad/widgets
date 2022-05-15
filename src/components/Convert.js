3905fddfdcf0e936ef3307e344f4b3ecfd842abe
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-15 14:58:13 +0300

    Implemented axios request to Google API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { apiKey } from "../apis";
 
 const Convert = ({ language, text }) => {
+const [translated, setTranslated] = useState('');
+
     useEffect(() => {
-        console.log('New language or text');
+        const doTranslation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: apiKey
+                }
+            });
+
+            setTranslated(data.data.translations[0].translatedText);
+        };
+
+        doTranslation();
+        
     }, [language, text]);
-    return <div />;
+
+    return (
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
+    );
 };
 
 export default Convert;
