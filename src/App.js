e16f793df19970136dfbbe1f5a7e1631955d0072
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-15 13:41:35 +0300

    Set up text input field and all corresponding props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
 // import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 // const items = [
 //     {
@@ -34,18 +35,10 @@ const options = [
 ];
 
 const App = () => {
-    const [selected, setSelected] = useState(options[0]);
-    const [showDropDown, setShowDropDown] = useState(true);
+
     return (
         <div>
-            <button onClick={() => setShowDropDown(!showDropDown)}>Toggle dropdown</button>
-            {showDropDown ?
-                <Dropdown
-                    selected={selected}
-                    onSelectedChange={setSelected}
-                    options={options}
-                /> : null
-            }
+            <Translate />
         </div>
     );
 };
