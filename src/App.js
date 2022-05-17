5d45188f74fac1348edd415aa8b2d9f61eabb7d9
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-17 17:40:59 +0300

    Added Route component for base routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
     {
@@ -34,38 +35,28 @@ const options = [
     },
 ];
 
-const showAccordion = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion items={items} />
-    }
-};
-
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search />
-    }
-};
-
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown />
-    }
-};
-
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-        return <Translate />
-    }
-};
-
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
 
     return (
         <div>
-            {showAccordion()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a Color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
